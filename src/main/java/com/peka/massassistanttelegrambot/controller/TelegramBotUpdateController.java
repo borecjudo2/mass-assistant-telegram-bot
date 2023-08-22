79e05bf96e211c6229dcc99e04d7135cd8a5f4bb
Vladislav Karpeka <Vlad.Karpeka@vertexinc.com>
2023-08-22 18:29:31 +0300

    Migrated threadExecutor on RxJava

--- src/main/java/com/peka/massassistanttelegrambot/controller/TelegramBotUpdateController.java
+++ src/main/java/com/peka/massassistanttelegrambot/controller/TelegramBotUpdateController.java
@@ -4,10 +4,13 @@ import com.peka.massassistanttelegrambot.exception.TelegramException;
 import com.peka.massassistanttelegrambot.exception.TelegramExceptionHandler;
 import com.peka.massassistanttelegrambot.queue.BotQueue;
 import com.peka.massassistanttelegrambot.service.BotService;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -26,18 +29,38 @@ public class TelegramBotUpdateController {
   private final BotService service;
   private final TelegramExceptionHandler exceptionHandler;
 
-  @Async("threadPoolTaskExecutor")
-  @Scheduled(fixedRate = 100)
-  public void processUpdate() {
-    try {
-      log.info("Just started processUpdate new iteration");
+  private Disposable disposable;
 
-      Update update = queue.takeUpdate();
+  @PostConstruct
+  public void init() {
+    Observable<Update> observable = Observable.create(emitter -> {
+      while (!emitter.isDisposed()) {
+        Update data = queue.takeUpdate();
+        emitter.onNext(data);
+      }
+    });
+
+    disposable = observable
+        .subscribeOn(Schedulers.io())
+        .observeOn(Schedulers.computation())
+        .subscribe(this::processUpdate);
+  }
+
+  private void processUpdate(Update update) {
+    try {
+      String threadName = String.format("%s-%s", Thread.currentThread().getName(), Thread.currentThread().getId());
+      log.info("Just started processUpdate new iteration " + threadName);
       service.processUpdate(update);
     } catch (TelegramException exception) {
       exceptionHandler.handle(exception);
     } catch (Exception exception) {
       log.error(exception.getLocalizedMessage(), exception);
     }
+  }
+
+  @PreDestroy
+  public void destroy() {
+    log.info("Destroying");
+    disposable.dispose();
   }
 }
